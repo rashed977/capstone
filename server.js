48f047db61a4876390818f3a37c13066c54d7175
Rashed Amer <rashedmalkawi97@gmail.com>
2022-09-07 23:15:28 +0300

    second

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ app.use(express.static(__dirname+'/dist/capstoneproj'))
 app.get('/*',(req, res)=>{
   res.sendFile(__dirname+'/dist/capstoneproj/index.html')
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 9080);
