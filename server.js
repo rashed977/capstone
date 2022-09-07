acd1a38a5675e562c420fea04fc4944c0749c011
Rashed Amer <rashedmalkawi97@gmail.com>
2022-09-07 22:25:52 +0300

    prefinal1

---
+++ server.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname+'/dist/capstoneproj'))
+app.get('/*',(req, res)=>{
+  res.sendFile(__dirname+'/dist/capstoneproj/index.html')
+});
+app.listen(process.env.PORT || 8080);
